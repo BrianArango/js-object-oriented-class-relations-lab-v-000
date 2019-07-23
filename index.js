b19dacb63cf9fdba383bb882669a2d16f0ad14aa
Brian Arango <brianvictus@gmail.com>
2019-07-23 19:21:42 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+let store = {driver: [], passengers: [], trips: []};
+
+let driverId = 0;
+let passengerId = 0;
+let tripId = 0;
+
+class Driver {
+  constructor(name) {
+    this.id = ++ driverId;
+    this.name = name;
+    store.users.push(this);
+  }
+  passengers() {
+    return //;
+  }
+    trips() {
+    return driver.ID;
+  }
+}
+
+
+class Passengers {
+  constructor(name) {
+    this.id = ++ 
+  }
+}
+
+class Trips {
+  constructor(name) {
+    this.id = ++ 
+  }
+}
